dd62b29dc7b254db4460609eeace54b28996a209
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2023-04-11 16:58:17 -0400

    origin offset on canvas

--- src/pages/arcanigen/definitions/collections/resultNode.tsx
+++ src/pages/arcanigen/definitions/collections/resultNode.tsx
@@ -15,11 +15,15 @@ interface IResultNode extends INodeDefinition {
       canvasColor: Color;
       canvasWidth: Length;
       canvasHeight: Length;
+      originOffsetX: Length;
+      originOffsetY: Length;
    };
    values: {
       canvasColor: Color;
       canvasWidth: Length;
       canvasHeight: Length;
+      originOffsetX: Length;
+      originOffsetY: Length;
    };
 }
 
@@ -30,9 +34,15 @@ const Controls = memo(({ nodeId, globals }: ControlRendererProps) => {
    const [canvasWidth, setCanvasWidth] = nodeHelper.useValueState(nodeId, "canvasWidth");
    const [canvasHeight, setCanvasHeight] = nodeHelper.useValueState(nodeId, "canvasHeight");
 
+   const [originOffsetX, setOriginOffsetX] = nodeHelper.useValueState(nodeId, "originOffsetX");
+   const [originOffsetY, setOriginOffsetY] = nodeHelper.useValueState(nodeId, "originOffsetY");
+
    const hasCanvasColor = nodeHelper.useHasLink(nodeId, "canvasColor");
    const hasCanvasWidth = nodeHelper.useHasLink(nodeId, "canvasWidth");
    const hasCanvasHeight = nodeHelper.useHasLink(nodeId, "canvasHeight");
+
+   const hasOriginOffsetX = nodeHelper.useHasLink(nodeId, "originOffsetX");
+   const hasOriginOffsetY = nodeHelper.useHasLink(nodeId, "originOffsetY");
 
    return (
       <BaseNode<IResultNode> nodeId={nodeId} helper={ResultNodeHelper} noRemove>
@@ -55,6 +65,16 @@ const Controls = memo(({ nodeId, globals }: ControlRendererProps) => {
                <LengthInput value={canvasHeight} onValidValue={setCanvasHeight} disabled={hasCanvasHeight} min={0} />
             </BaseNode.Input>
          </SocketIn>
+         <SocketIn<IResultNode> type={SocketTypes.LENGTH} nodeId={nodeId} socketId={"originOffsetX"}>
+            <BaseNode.Input label={"origin Offset X"}>
+               <LengthInput value={originOffsetX} onValidValue={setOriginOffsetX} disabled={hasOriginOffsetX} />
+            </BaseNode.Input>
+         </SocketIn>
+         <SocketIn<IResultNode> type={SocketTypes.LENGTH} nodeId={nodeId} socketId={"originOffsetY"}>
+            <BaseNode.Input label={"Origin Offset Y"}>
+               <LengthInput value={originOffsetY} onValidValue={setOriginOffsetY} disabled={hasOriginOffsetY} />
+            </BaseNode.Input>
+         </SocketIn>
       </BaseNode>
    );
 });
@@ -70,6 +90,8 @@ const ResultNodeHelper: INodeHelper<IResultNode> = {
       canvasColor: { r: 1, g: 1, b: 1, a: 1 },
       canvasWidth: { value: 800, unit: "px" },
       canvasHeight: { value: 800, unit: "px" },
+      originOffsetX: { value: 0, unit: "px" },
+      originOffsetY: { value: 0, unit: "px" },
    }),
    controls: Controls,
 };
@@ -83,6 +105,11 @@ export const RootNodeRenderer = () => {
    const canvasHeight = nodeHelper.useCoalesce("ROOT", "canvasHeight", "canvasHeight", globals);
    const canvasWidth = nodeHelper.useCoalesce("ROOT", "canvasWidth", "canvasWidth", globals);
 
+   const originOffsetX = nodeHelper.useCoalesce("ROOT", "canvasHeight", "canvasHeight", globals);
+   const originOffsetY = nodeHelper.useCoalesce("ROOT", "canvasWidth", "canvasWidth", globals);
+
+   const x = useMemo(() => MathHelper.lengthToPx(originOffsetX ?? { value: 0, unit: "px" }), [originOffsetX]);
+   const y = useMemo(() => MathHelper.lengthToPx(originOffsetY ?? { value: 0, unit: "px" }), [originOffsetY]);
    const h = useMemo(() => Math.max(0, MathHelper.lengthToPx(canvasHeight ?? { value: 800, unit: "px" })), [canvasHeight]);
    const w = useMemo(() => Math.max(0, MathHelper.lengthToPx(canvasWidth ?? { value: 800, unit: "px" })), [canvasWidth]);
 
@@ -97,7 +124,7 @@ export const RootNodeRenderer = () => {
          xmlns="http://www.w3.org/2000/svg"
          xmlnsXlink="http://www.w3.org/1999/xlink"
       >
-         {OutputRenderer && childNodeId && <OutputRenderer nodeId={childNodeId} depth={"ROOT"} globals={globals} />}
+         <g transform={`translate(${x}, ${y}`}>{OutputRenderer && childNodeId && <OutputRenderer nodeId={childNodeId} depth={"ROOT"} globals={globals} />}</g>
       </svg>
    );
 };
